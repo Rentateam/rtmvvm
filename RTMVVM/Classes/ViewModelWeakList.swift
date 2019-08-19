432fd68de0bd4dfe05ff58e038e692a7d5fc7619
a-25 <anuryadov@gmail.com>
2019-08-19 17:31:51 +0300

    Restored ViewModelWeakList

---
+++ RTMVVM/Classes/ViewModelWeakList.swift
@@ -0,0 +1,20 @@
+//Copyright © 2018 Rentateam. All rights reserved.
+import Foundation
+
+public class ViewModelWeakList<T: AnyObject> {
+    private var modelList: NSHashTable<T>
+    
+    public init() {
+        self.modelList = NSHashTable<T>.weakObjects()
+    }
+    
+    public func addViewModel(model: T) {
+        if !self.modelList.contains(model) {
+            self.modelList.add(model)
+        }
+    }
+    
+    public func getList() -> [T] {
+        return self.modelList.allObjects
+    }
+}
